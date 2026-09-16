bd3df8412365804ce662a36240cf9c1c525a62bd
doug-dotcom <doug@struthersfinancial.com.au>
2026-09-16 20:41:42 +0800

    Activate Layer 12 deep recall evidence breadth

--- layers/layer3_family_recall.py
+++ layers/layer3_family_recall.py
@@ -91,3 +91,5 @@ def install(rhee):
     install_career_stage_coverage_repair(rhee)
     from layers.layer11_true_deep_recall import install as install_true_deep_recall
     install_true_deep_recall(rhee)
+    from layers.layer12_deep_recall_evidence_breadth import install as install_deep_recall_evidence_breadth
+    install_deep_recall_evidence_breadth(rhee)
